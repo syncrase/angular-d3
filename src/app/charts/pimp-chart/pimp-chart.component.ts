495014d967c1b40f0d4947efd53e7955750586af
syncrase <pierre.taquet@gmail.com>
2020-10-04 14:39:20 +0200

    fix positionnement tooltip

--- src/app/charts/pimp-chart/pimp-chart.component.ts
+++ src/app/charts/pimp-chart/pimp-chart.component.ts
@@ -10,7 +10,6 @@ import { legendColor } from 'd3-svg-legend';
 })
 export class PimpChartComponent implements OnInit {
 
-
   readonly svgDimensions = { width: 800, height: 500 };
   readonly margin = { left: 5, right: 5, top: 10, bottom: 10 };
   readonly chartDimensions = {
@@ -18,87 +17,72 @@ export class PimpChartComponent implements OnInit {
     height: this.svgDimensions.height - this.margin.bottom - this.margin.top
   };
 
-
   partiesColor = [];
-
-  readonly svg = d3
-    .select('body')
-    .append('svg')
-    .attr('width', this.svgDimensions.width)
-    .attr('height', this.svgDimensions.height)
-    .attr('style', 'background-color: #FBFAF0');
-
-  readonly chartGroup = this.svg
-    .append('g')
-    // .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
-    .attr('width', this.chartDimensions.width)
-    .attr('height', this.chartDimensions.height);
 
   readonly rayon = Math.min(this.chartDimensions.width, this.chartDimensions.height) / 2;
 
-
-  arcs: d3.Selection<d3.EnterElement, d3.PieArcDatum<number | { valueOf(): number; }>, SVGGElement, unknown>;
-  div =
-    d3
-      .select('body')
-      // this.svg
-      .append('div')
-      .attr('class', 'tooltip')
-      // .attr('height', '100px')
-      // .attr('width', '100px')
-      // .attr('style', 'background-color: red')// .attr style doit être AVANT .style
-      // .style('height', '100px')
-      // .style('width', '100px')
-      // .style('z-index', '9999')
-      // .style("position", "absolute")
-      .attr('left', `500px`)// d3.event.pageX
-      .attr('top', `500px`)// d3.event.pageY;
-      // .attr('transform', `translate(200, 200)`)
-      // .text(
-      //   `<span>olala</span>`
-      // );
-      .text(
-        `I'm at the wrong place!`
-      );
-  // .style('height', '100px')
-  // .style('width', '100px');
-  // .text('hello');
-  // .style('opacity', 0)
-  // .style('left', `500px`)// d3.event.pageX
-  // .style('top', `500px`);// d3.event.pageY;
-  // .attr('transform', `translate(500,500)`);
-
+  arcs: any;
+  tooltip: any;
+  /**
+   * Contain all graph and tooltip
+   */
+  container: any;
+  /**
+   * Contain pie chart and legend
+   */
+  graphGroup: any;
+  pieChart: any;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.container = d3.select('app-pimp-chart')
+      .append('div')
+      .attr('class', 'container')
+      .style('position', 'relative');
+
+    // Init graph group
+    this.graphGroup = this.container
+      .append('svg')
+      .attr('width', this.svgDimensions.width)
+      .attr('height', this.svgDimensions.height)
+      .attr('style', 'background-color: #FBFAF0');
+
+    this.tooltip =
+      this.container
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('position', 'absolute')
+        .style('left', `500px`)
+        .style('top', `200px`);
+
     this.constructArc();
     this.genererLesCouleursDesEntrees();
     this.createSvg();
     this.createLengend();
-
   }
 
   constructArc() {
-
-
-    const pieChart = d3
+    const pieChartFormatting = d3
       .pie()
-      // .startAngle(-90 * (Math.PI / 180))
       .startAngle(Math.PI * 0) // sin = 1 et cos = 0
-      // .endAngle(90 * (Math.PI / 180));
       .endAngle(Math.PI * 1); // sin = -1 et cos = 0
+    const pie = pieChartFormatting(siegesParPartiNov19.map(result => result.sieges));
 
-    const pie = pieChart(siegesParPartiNov19.map(result => result.sieges));
+    this.pieChart = this.graphGroup
+      .append('g')
+      .attr('width', this.chartDimensions.width)
+      .attr('height', this.chartDimensions.height);
 
-    this.arcs = this.chartGroup
+    this.arcs = this.pieChart
       .selectAll('slice')
       .data(pie)
       .enter();
   }
 
   createSvg() {
-    this.chartGroup.attr('transform', `translate(${this.rayon * 2},${this.rayon})`);
+    this.pieChart.attr('transform', `translate(${this.rayon * 2},${this.rayon})`);
 
     const arc = d3
       .arc()
@@ -108,11 +92,13 @@ export class PimpChartComponent implements OnInit {
     this.arcs
       .append('path')
       .text(function (d) { return 'test'; })
-      .attr('d', <any>arc) // Hack typing: https://stackoverflow.com/questions/35413072/compilation-errors-when-drawing-a-piechart-using-d3-js-typescript-and-angular/38021825
-      .attr('fill', (d, i) => this.partiesColor[i]) // TODO color ordinal
-      .on('mouseover', this.leCurseurSurvoleLaZone)
-      .on('mouseout', this.leCurseurSortDeLaZone);
+      .attr('d', <any>arc)
+      .attr('fill', (d: any, i: any) => this.partiesColor[i])
+      // .attr('pointer-events', 'all')
+      .on('mouseover', this.onMouseover)
+      .on('mouseout', this.onMouseOut);
 
+    // Add label at the middle of the section
     this.arcs.append('text')
       .attr('transform', function (d) {
         const coordonneesDuCentreDeLarc = arc.centroid(<d3.DefaultArcObject><unknown>d);
@@ -128,9 +114,8 @@ export class PimpChartComponent implements OnInit {
       .scaleOrdinal(this.partiesColor)
       .domain(siegesParPartiNov19.map(parti => parti.nom));
 
-    this.svg
+    this.graphGroup
       .append('g')
-      // ${this.rayon - 20}
       .attr('transform', `translate(${this.margin.left}, 0)`)
       .call(legendColor().scale(ordinal))
   }
@@ -148,7 +133,13 @@ export class PimpChartComponent implements OnInit {
     return color;
   }
 
-  leCurseurSurvoleLaZone(d: any, i: any, n: any) {
+  readonly onMouseover = (d: any, i: any) => {
+    // Be aware of the scope : 'this' is only accessible in an arrow function
+    const e = this.arcs.nodes();
+    // const index = e.indexOf(this); // --> Argument of type 'this' is not assignable to parameter of type 'EnterElement'
+    const index = e.indexOf(e.find(elt => this.shallowEqual((<any>elt).__data__, i)));
+    // const pointerEvent = d3.pointer(d) // --> Property 'pointer' does not exist on type 'typeof import("<project_root>/node_modules/@types/d3/index")'
+
     d3.select(d.currentTarget)
       .transition()
       .duration(200)
@@ -159,31 +150,47 @@ export class PimpChartComponent implements OnInit {
       // html() doit se situer AVANT la transition
       .html(
         `<span>${siegesParPartiNov19[i.index].nom}: ${siegesParPartiNov19[i.index].sieges}</span>`
-      );
-    // .transition()
-    // .duration(200)
-    // .style('opacity', 0.9)
-    // .style('left', `500px`)// d3.event.pageX
-    // .style('top', `${d.clientY - 28}px`)// d3.event.pageY;
-
-    // .style('height', `50px`)
-    // .style('width', `100px`);
-
+      )
+      .transition()
+      .duration(200)
+      .style('opacity', 0.9)
+      .style('left', (a, b, c: any) => {
+        // c: Array { div.tooltip (HtmlElement)}
+        return `${this.rayon * 2 - c[0].clientWidth / 2}px`;
+      })
+      .style('top', `${this.rayon}px`);
   }
 
-  /**
-   *
-   */
-  leCurseurSortDeLaZone(d: any, i: any) {
+  readonly onMouseOut = (d: any, i: any) => {
     d3.select(d.currentTarget)
       .transition()
       .duration(200)
       .style('opacity', 1)
       .attr('transform', ``);
 
-    // d3.select('.tooltip')
-    // .transition()
-    // .duration(200)
-    // .style('opacity', 0);
+    d3.select('.tooltip')
+      .transition()
+      .duration(200)
+      .style('opacity', 0);
   }
+
+
+  shallowEqual(object1: { [x: string]: any; }, object2: { [x: string]: any; }) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+
 }
